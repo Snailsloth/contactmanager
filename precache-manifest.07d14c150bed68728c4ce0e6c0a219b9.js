81d2b12fa80996b1bc89aecc02df0545a63c3cf4
Snsl <oleisme@gmail.com>
2019-05-04 17:56:37 +0300

    Updates

---
+++ precache-manifest.07d14c150bed68728c4ce0e6c0a219b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21aa0c92c1a4ba7b94d9",
+    "url": "/contactmanager/static/css/main.7863f6b6.chunk.css"
+  },
+  {
+    "revision": "21aa0c92c1a4ba7b94d9",
+    "url": "/contactmanager/static/js/main.cecadacd.chunk.js"
+  },
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.4bdee9f6.js"
+  },
+  {
+    "revision": "d4d9fe800fb155e75dca",
+    "url": "/contactmanager/static/js/2.5801b4a6.chunk.js"
+  },
+  {
+    "revision": "c5c04bafb12b13594039ab192857c22d",
+    "url": "/contactmanager/index.html"
+  }
+];
